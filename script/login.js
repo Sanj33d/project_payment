7ee8556ca6442b93b3fba861fe0c4ac2aeb3a133
Sanj33d <sanjeedsajeen8@gmail.com>
2025-02-28 08:31:12 +0600

    login event and nav done

---
+++ script/login.js
@@ -0,0 +1,19 @@
+document.getElementById("btn-login").addEventListener("click", function(event){
+    event.preventDefault()
+    const inputNumber = document.getElementById("input-number").value;
+    const inputPin = document.getElementById("input-pin").value;
+
+    // console.log(inputNumber, inputPin);
+    if (inputNumber.length===4){
+        if (parseInt(inputPin)===2358){
+            window.location.href="login.html";
+        }
+        else{
+            alert("Pin incorrect")
+        }
+    }
+    else{
+        alert("Account Number incorrect")
+    }
+
+})
