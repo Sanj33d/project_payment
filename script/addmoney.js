8cb2220e5060f9ab17635d2a6a2098cf1914bec6
Sanj33d <sanjeedsajeen8@gmail.com>
2025-03-04 14:24:00 +0600

    transaction history js complete

--- script/addmoney.js
+++ script/addmoney.js
@@ -1,22 +1,37 @@
 document.getElementById("form-cashout").style.display = "none";
+document.getElementById("transaction-history").style.display = "none";
 
-// ui (add money, cashout)
+// clicking 'add money' button
 
 document.getElementById("box-cashout").addEventListener("click", function(){
     document.getElementById("form-addmoney").style.display = "none";
     document.getElementById("form-cashout").style.display = "block";
+    document.getElementById("transaction-history").style.display = "none";
 
     this.classList.add("bg-pink-100")
     document.getElementById("box-addmoney").classList.remove("bg-pink-100")
+    document.getElementById("btn-transaction").classList.remove("bg-pink-100")
 })
 
+// clicking button 'cashout'
 document.getElementById("box-addmoney").addEventListener("click", function(){
     document.getElementById("form-cashout").style.display = "none";
     document.getElementById("form-addmoney").style.display = "block";
+    document.getElementById("transaction-history").style.display = "none";
 
     this.classList.add("bg-pink-100")
     document.getElementById("box-cashout").classList.remove("bg-pink-100")
+    document.getElementById("btn-transaction").classList.remove("bg-pink-100")
 
+})
+// clicking button 'transaction'
+
+document.getElementById("btn-transaction").addEventListener("click", function(){
+    document.getElementById("transaction-history").style.display = "block";
+    this.classList.add("bg-pink-100")
+    
+    document.getElementById("box-cashout").classList.remove("bg-pink-100")
+    document.getElementById("box-addmoney").classList.remove("bg-pink-100")
 })
 
 // adding money by clicking 'add money' button
@@ -39,6 +54,16 @@ document.getElementById("btn-addmoney").addEventListener("click", function(event
 
             document.getElementById("balance").innerText = sum
 
+            // transaction history
+            let bank = document.getElementById("inp-bank").value
+
+            let p = document.createElement("p")
+            p.classList.add("text-center")
+            p.innerText = `
+            ${amount} taka added from ${bank} successfully`
+
+            let parent = document.getElementById("transaction-history")
+            parent.appendChild(p)
         }
         else{
             alert("Pin incorrect")
--- script/cashout.js
+++ script/cashout.js
@@ -15,6 +15,15 @@ document.getElementById("btn-cashout").addEventListener("click", function(event)
 
             sum = balance - amount
             document.getElementById("balance").innerText = sum
+
+            // transaction history
+            let p = document.createElement("p")
+            p.classList.add("text-center")
+            p.innerText = `
+            ${amount} taka cash out successful`
+
+            let parent = document.getElementById("transaction-history")
+            parent.appendChild(p)
         }
         else{
             alert("Pin incorrect")
